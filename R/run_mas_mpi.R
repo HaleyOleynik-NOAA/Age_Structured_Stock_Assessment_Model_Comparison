003dfb2f92ba92b7cb7893cc3d02240d289916fd
Haley Oleynik <h.oleynik@oceans.ubc.ca>
2022-04-05 10:52:32 -0700

    Update run_mas_mpi.R

--- R/run_mas_mpi.R
+++ R/run_mas_mpi.R
@@ -1,48 +1,31 @@
-#' Function to run Metapopulation Assessment System (https://github.com/nmfs-fish-tools/r4MAS)
-#' @name run_mas
-#' @description Function to read simulated true values from the operating model and to run MAS
-#' @param maindir Main working directory
-#' @param subdir Estimation model working directory
-#' @param om_sim_num Number of iterations from the operating model
-#' @param casedir Case working directory
-#' @param em_bias_cor Use bias correction in the estimation model or not?
 #' @export
-
-.Last <- function(){
-  if (is.loaded("mpi_initialize")){
-    if (mpi.comm.size(1) > 0){
-      print("Please use mpi.close.Rslaves() to close slaves.")
-      mpi.close.Rslaves()
-    }
-    print("Please use mpi.quit() to quit R")
-    .Call("mpi_finalize")
-  }
-}
-
-run_mas <- function(
-  maindir = maindir, 
-  subdir = "MAS", 
-  om_sim_num = NULL, 
-  casedir = casedir, 
-  em_bias_cor = em_bias_cor, 
-  input_filename = NULL) {
+run_mas_segment <- function(maindir = maindir,
+                            subdir = "MAS",
+                            om_sim_num = NULL,
+                            casedir = casedir,
+                            em_bias_cor = em_bias_cor,
+                            input_filename = NULL,
+                            begin,
+                            end) {
   
-  if (!("r4MAS" %in% installed.packages()[, "Package"])) stop("Please install r4MAS!")
+  if (!("r4MAS" %in% installed.packages()[, "Package"]))
+    stop("Please install r4MAS!")
   
   setwd(file.path(casedir, "output", subdir))
   unlink(list.files(file.path(casedir, "output", "MAS"), full.names = TRUE), recursive = TRUE)
-  sapply(1:om_sim_num, function(x) dir.create(file.path(casedir, "output", subdir, paste("s", x, sep = ""))))
- 
-  library(Rmpi)
-  ns <- 3
-
-begin = 0
-end = 0
-mpi.irecv(begin, tag = mpi.any.tag, source = 0)
-mpi.irecv(end, tag = mpi.any.tag, source = 0)
-
-  for (om_sim in begin:end) {
-    load(file=file.path(casedir, "output", "OM", paste("OM", om_sim, ".RData", sep="")))
+  sapply(1:om_sim_num, function(x)
+    dir.create(file.path(
+      casedir, "output", subdir, paste("s", x, sep = "")
+    )))
+  
+  
+  for (om_sim in begin[mpi.comm.rank()]:end[mpi.comm.rank()]) {
+    load(file = file.path(
+      casedir,
+      "output",
+      "OM",
+      paste("OM", om_sim, ".RData", sep = "")
+    ))
     
     library(r4MAS)
     r4mas <- Rcpp::Module("rmas", PACKAGE = "r4MAS")
@@ -85,7 +68,7 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
     growth$SetUndifferentiatedSurveyWeight(survey_empirical_weight)
     
     maturity <- new(r4mas$Maturity)
-    maturity$values <- om_input$mat.age 
+    maturity$values <- om_input$mat.age
     
     natural_mortality <- new(r4mas$NaturalMortality)
     natural_mortality$SetValues(om_input$M.age)
@@ -116,11 +99,13 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
     # Catch index values and observation errors
     catch_index <- new(r4mas$IndexData)
     catch_index$values <- em_input$L.obs$fleet1
-    catch_index$error <- rep(em_input$cv.L$fleet1, times = om_input$nyr)
+    catch_index$error <-
+      rep(em_input$cv.L$fleet1, times = om_input$nyr)
     # Catch composition data
     catch_comp <- new(r4mas$AgeCompData)
     catch_comp$values <- as.vector(t(em_input$L.age.obs$fleet1))
-    catch_comp$sample_size <- rep(em_input$n.L$fleet1, nyears * nseasons)
+    catch_comp$sample_size <-
+      rep(em_input$n.L$fleet1, nyears * nseasons)
     # Likelihood component settings
     fleet_index_comp_nll <- new(r4mas$Lognormal)
     fleet_index_comp_nll$use_bias_correction <- FALSE
@@ -128,12 +113,14 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
     # Fleet selectivity settings
     if (om_input$sel_fleet$fleet1$pattern == 1) {
       fleet_selectivity <- new(r4mas$LogisticSelectivity)
-      fleet_selectivity$a50$value <- om_input$sel_fleet$fleet1$A50.sel
+      fleet_selectivity$a50$value <-
+        om_input$sel_fleet$fleet1$A50.sel
       fleet_selectivity$a50$estimated <- TRUE
       fleet_selectivity$a50$phase <- 2
       fleet_selectivity$a50$min <- 0.0
       fleet_selectivity$a50$max <- max(om_input$ages)
-      fleet_selectivity$slope$value <- 1 / om_input$sel_fleet$fleet1$slope.sel
+      fleet_selectivity$slope$value <-
+        1 / om_input$sel_fleet$fleet1$slope.sel
       fleet_selectivity$slope$estimated <- TRUE
       fleet_selectivity$slope$phase <- 2
       fleet_selectivity$slope$min <- 0.0001
@@ -143,25 +130,29 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
     if (om_input$sel_fleet$fleet1$pattern == 2) {
       fleet_selectivity <- new(r4mas$DoubleLogisticSelectivity)
       
-      fleet_selectivity$alpha_asc$value <- om_input$sel_fleet$fleet1$A50.sel1
+      fleet_selectivity$alpha_asc$value <-
+        om_input$sel_fleet$fleet1$A50.sel1
       fleet_selectivity$alpha_asc$estimated <- TRUE
       fleet_selectivity$alpha_asc$phase <- 2
       fleet_selectivity$alpha_asc$min <- 0.0
       fleet_selectivity$alpha_asc$max <- max(om_input$ages)
       
-      fleet_selectivity$beta_asc$value <- om_input$sel_fleet$fleet1$slope.sel1
+      fleet_selectivity$beta_asc$value <-
+        om_input$sel_fleet$fleet1$slope.sel1
       fleet_selectivity$beta_asc$estimated <- TRUE
       fleet_selectivity$beta_asc$phase <- 2
       fleet_selectivity$beta_asc$min <- 0.0001
       fleet_selectivity$beta_asc$max <- max(om_input$ages)
       
-      fleet_selectivity$alpha_desc$value <- om_input$sel_fleet$fleet1$A50.sel2
+      fleet_selectivity$alpha_desc$value <-
+        om_input$sel_fleet$fleet1$A50.sel2
       fleet_selectivity$alpha_desc$estimated <- TRUE
       fleet_selectivity$alpha_desc$phase <- 2
       fleet_selectivity$alpha_desc$min <- 0.0
       fleet_selectivity$alpha_desc$max <- max(om_input$ages)
       
-      fleet_selectivity$beta_desc$value <- om_input$sel_fleet$fleet1$slope.sel2
+      fleet_selectivity$beta_desc$value <-
+        om_input$sel_fleet$fleet1$slope.sel2
       fleet_selectivity$beta_desc$estimated <- TRUE
       fleet_selectivity$beta_desc$phase <- 2
       fleet_selectivity$beta_desc$min <- 0.0001
@@ -187,28 +178,35 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
     fleet$AddFishingMortality(fishing_mortality$id, 1, area1$id)
     
     # Survey index values and observation errors
-    survey_index <- vector(mode = "list", length = om_input$survey_num)
+    survey_index <-
+      vector(mode = "list", length = om_input$survey_num)
     
     for (i in 1:om_input$survey_num) {
       survey_index[[i]] <- new(r4mas$IndexData)
       # survey_index[[i]]$id <- i
       survey_index[[i]]$values <- em_input$survey.obs[[i]]
-      survey_index[[i]]$error <- rep(em_input$cv.survey[[i]], times = om_input$nyr)
+      survey_index[[i]]$error <-
+        rep(em_input$cv.survey[[i]], times = om_input$nyr)
     }
     
     # Survey composition
-    survey_comp <- vector(mode = "list", length = om_input$survey_num)
+    survey_comp <-
+      vector(mode = "list", length = om_input$survey_num)
     for (i in 1:om_input$survey_num) {
       survey_comp[[i]] <- new(r4mas$AgeCompData)
       # survey_comp[[i]]$id <- i
-      survey_comp[[i]]$values <- as.vector(t(em_input$survey.age.obs[[i]]))
-      survey_comp[[i]]$sample_size <- rep(em_input$n.survey[[i]], times = om_input$nyr)
+      survey_comp[[i]]$values <-
+        as.vector(t(em_input$survey.age.obs[[i]]))
+      survey_comp[[i]]$sample_size <-
+        rep(em_input$n.survey[[i]], times = om_input$nyr)
     }
     
     # Likelihood component settings
     
-    survey_index_comp_nll <- vector(mode = "list", length = om_input$survey_num)
-    survey_age_comp_nll <- vector(mode = "list", length = om_input$survey_num)
+    survey_index_comp_nll <-
+      vector(mode = "list", length = om_input$survey_num)
+    survey_age_comp_nll <-
+      vector(mode = "list", length = om_input$survey_num)
     for (i in 1:om_input$survey_num) {
       survey_index_comp_nll[[i]] <- new(r4mas$Lognormal)
       survey_index_comp_nll[[i]]$use_bias_correction <- FALSE
@@ -219,7 +217,8 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
     }
     
     # Survey selectivity settings
-    survey_selectivity <- vector(mode = "list", length = om_input$survey_num)
+    survey_selectivity <-
+      vector(mode = "list", length = om_input$survey_num)
     
     for (i in 1:om_input$survey_num) {
       selectivity_option <- om_input$sel_survey[[i]]$pattern
@@ -227,13 +226,15 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
       if (selectivity_option == 1) {
         survey_selectivity[[i]] <- new(r4mas$LogisticSelectivity)
         # survey_selectivity[[i]]$id <- i
-        survey_selectivity[[i]]$a50$value <- om_input$sel_survey[[i]]$A50.sel
+        survey_selectivity[[i]]$a50$value <-
+          om_input$sel_survey[[i]]$A50.sel
         survey_selectivity[[i]]$a50$estimated <- TRUE
         survey_selectivity[[i]]$a50$phase <- 2
         survey_selectivity[[i]]$a50$min <- 0
         survey_selectivity[[i]]$a50$max <- max(om_input$ages)
         
-        survey_selectivity[[i]]$slope$value <- 1 / om_input$sel_survey[[i]]$slope.sel
+        survey_selectivity[[i]]$slope$value <-
+          1 / om_input$sel_survey[[i]]$slope.sel
         survey_selectivity[[i]]$slope$estimated <- TRUE
         survey_selectivity[[i]]$slope$phase <- 2
         survey_selectivity[[i]]$slope$min <- 0.0001
@@ -244,25 +245,29 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
         survey_selectivity[[i]] <- new(r4mas$DoubleLogisticSelectivity)
         # survey_selectivity[[i]]$id <- i
         
-        survey_selectivity[[i]]$alpha_asc$value <- om_input$sel_survey[[i]]$A50.sel1
+        survey_selectivity[[i]]$alpha_asc$value <-
+          om_input$sel_survey[[i]]$A50.sel1
         survey_selectivity[[i]]$alpha_asc$estimated <- TRUE
         survey_selectivity[[i]]$alpha_asc$phase <- 2
         survey_selectivity[[i]]$alpha_asc$min <- 0.0
         survey_selectivity[[i]]$alpha_asc$max <- max(om_input$ages)
         
-        survey_selectivity[[i]]$beta_asc$value <- om_input$sel_survey[[i]]$slope.sel1
+        survey_selectivity[[i]]$beta_asc$value <-
+          om_input$sel_survey[[i]]$slope.sel1
         survey_selectivity[[i]]$beta_asc$estimated <- TRUE
         survey_selectivity[[i]]$beta_asc$phase <- 2
         survey_selectivity[[i]]$beta_asc$min <- 0.0001
         survey_selectivity[[i]]$beta_asc$max <- max(om_input$ages)
         
-        survey_selectivity[[i]]$alpha_desc$value <- om_input$sel_survey[[i]]$A50.sel2
+        survey_selectivity[[i]]$alpha_desc$value <-
+          om_input$sel_survey[[i]]$A50.sel2
         survey_selectivity[[i]]$alpha_desc$estimated <- TRUE
         survey_selectivity[[i]]$alpha_desc$phase <- 2
         survey_selectivity[[i]]$alpha_desc$min <- 0.0
         survey_selectivity[[i]]$alpha_desc$max <- max(om_input$ages)
         
-        survey_selectivity[[i]]$beta_desc$value <- om_input$sel_survey[[i]]$slope.sel2
+        survey_selectivity[[i]]$beta_desc$value <-
+          om_input$sel_survey[[i]]$slope.sel2
         survey_selectivity[[i]]$beta_desc$estimated <- TRUE
         survey_selectivity[[i]]$beta_desc$phase <- 2
         survey_selectivity[[i]]$beta_desc$min <- 0.0001
@@ -291,7 +296,7 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
     }
     
     mas_model <- new(r4mas$MASModel)
-    mas_model$compute_variance_for_derived_quantities<-FALSE
+    mas_model$compute_variance_for_derived_quantities <- FALSE
     mas_model$nyears <- nyears
     mas_model$nseasons <- nseasons
     mas_model$nages <- nages
@@ -309,8 +314,83 @@ mpi.irecv(end, tag = mpi.any.tag, source = 0)
     
     # Run MAS
     mas_model$Run()
-    output_file <- file.path(casedir, "output", subdir, paste("s", om_sim, sep = ""), paste("s", om_sim, ".json", sep = ""))
+    output_file <-
+      file.path(
+        casedir,
+        "output",
+        subdir,
+        paste("s", om_sim, sep = ""),
+        paste("s", om_sim, ".json", sep = "")
+      )
     write(mas_model$GetOutput(), file = toString(output_file))
     mas_model$Reset()
   }
+  return (c(begin, end))
+}
+
+
+#' @export
+run_mas_mpi <- function(maindir = maindir,
+                    subdir = "MAS",
+                    om_sim_num = NULL,
+                    casedir = casedir,
+                    em_bias_cor = em_bias_cor,
+                    input_filename = NULL) {
+  print("Running MAS in MPI mode.....now")
+  
+  id <- mpi.comm.rank(comm = 0)
+  ns <- 5# mpi.universe.size() - 1
+  
+  nsims <- 120
+  
+  begin <- rep(0, ns)
+  end <- rep(0, ns)
+  #create scenario segments
+  print(paste("id = ", id))
+  if (id == 0) {
+    segments <- nsims / ns
+    for (i in 1:ns) {
+      if (i < ns) {
+        begin[i] <- (i - 1) * segments
+        end[i] <- i * segments
+      } else{
+        begin[i] <- (i - 1) * segments
+        end[i] <- nsims
+      }
+    }
+    
+    print("MPI segments:")
+    begin
+    end
+    print("launching children....")
+    mpi.spawn.Rslaves(nslaves = ns)
+    
+    #pass the function to all slaves
+    mpi.bcast.Robj2slave(obj = run_mas_segment)
+    
+    
+    
+    print("broadcasting run...")
+ 
+    #execute all slaves
+    x <- mpi.remote.exec(
+      run_mas_segment,
+      maindir,
+      subdir = "MAS",
+      om_sim_num,
+      casedir,
+      em_bias_cor,
+      input_filename,
+      begin,
+      end
+    )
+    print("done.")
+    #mpi.wait(request=1)
+    # Tell all slaves to close down, and exit the program
+    mpi.close.Rslaves(dellog = FALSE)
+    mpi.quit()
+  }
+  
+  
+  
 }
